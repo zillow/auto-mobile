f4ce9051e32903c12a2ce56a5790cb47cab65081
José Antonio Arellano Mendoza <antonioarmen@MacBook-Pro-de-Jose.local>
2025-07-22 13:48:29 -0600

    tab view

---
+++ ios/playground/Playground/Discover/DiscoverView.swift
@@ -0,0 +1,17 @@
+//
+//  DiscoverView.swift
+//  Playground
+//
+//  Created by José Antonio Arellano Mendoza on 22/07/25.
+//
+
+import SwiftUI
+
+struct DiscoverView: View {
+  var body: some View {
+    NavigationStack {
+      Text("Discover view")
+        .navigationTitle(Text("Discover"))
+    }
+  }
+}
---
+++ ios/playground/Playground/Settings/SettingsView.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsView.swift
+//  Playground
+//
+//  Created by José Antonio Arellano Mendoza on 22/07/25.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+  var body: some View {
+    NavigationStack {
+      Text("Settings view")
+        .navigationTitle(Text("Settings"))
+    }
+  }
+}
