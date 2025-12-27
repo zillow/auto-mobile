1983b3bf0908436e64896e991c47ea954429b744
Jason Pearson <jason.d.pearson@gmail.com>
2025-12-27 09:23:38 -0600

    Remove iOS WebDriverAgent git submodule

--- ios/playground/Playground/Views/MediaView/MediaView.swift
+++
@@ -1,9 +0,0 @@
-import AVKit
-import SwiftUI
-
-struct MediaView: View {
-  var body: some View {
-    VideoPlayer(player: AVPlayer(url:  Bundle.main.url(forResource: "video", withExtension: "mp4")!))
-        .frame(height: 400)
-  }
-}
