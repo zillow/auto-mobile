1fec71517cc375eba7e1fee7e5711ffdb5838c0d
Jason Pearson <jason.d.pearson@gmail.com>
2026-01-14 08:51:46 -0600

    clean slate ide plugin ux

--- android/ide-plugin/src/main/kotlin/dev/jasonpearson/automobile/ide/AutoMobileToolWindowFactory.kt
+++ android/ide-plugin/src/main/kotlin/dev/jasonpearson/automobile/ide/AutoMobileToolWindowFactory.kt
@@ -4,12 +4,12 @@ import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
-import org.jetbrains.jewel.bridge.JewelComposePanel
+import javax.swing.JPanel
 
 class AutoMobileToolWindowFactory : ToolWindowFactory, DumbAware {
   override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
     val content = toolWindow.contentManager.factory.createContent(
-      JewelComposePanel { AutoMobileToolWindowContent(project) },
+      JPanel(),
       "",
       false
     )
